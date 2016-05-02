3aa77006a821a031b5a945bdaa4e9f8208d56bd7
Varun Agrawal <varagrawal@gmail.com>
2016-05-02 17:55:52 -0400

    added quote feature on main page

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,22 @@
 console.log('This would be the main JS file.');
+
+function getQuote(){
+    console.log("Getting quote");
+    $.ajax({
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function(data) {
+            // Data is an array of size 1, so get the first one.
+            var quote = data.shift();
+
+            // add the quote to the webpage
+            $("#quote").html(quote.content + "\n - " + quote.title);
+        },
+        cache: false
+    });
+}
+
+$(function(){
+    $("[data-toggle='tooltip']").tooltip();
+
+    getQuote();
+});
